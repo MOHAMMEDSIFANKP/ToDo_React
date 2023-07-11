6e171c397839a4b19e61f984bd843e422c581e33
sifan007sifu@gmail.com <sifan007sifu@gmail.com>
2023-07-11 21:38:54 +0530

    First react app TODO app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
 function App() {
+  const [toDos,setTodos] = useState([])
+  const [toDO,setTodo] = useState('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input value={toDO} onChange={(e)=>setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>setTodos([...toDos,{id:Date.now(),text:toDO,status:false}])} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+       { toDos.map((obj)=>{
+       return(<div className="todo">
+          <div className="left">
+            <input onChange={(e)=>{
+              console.log(e.target.checked)
+              console.log(obj)
+              setTodo(toDos.filter(obj2=>{
+                if(obj2.id===obj.id){
+                  obj2.status=e.target.checked
+                }
+                return obj2
+              }))
+            }} value={obj.status} type="checkbox" name="" id="" />
+            <p>{obj.text}</p>
+          </div>
+          <div className="right">
+            <i className="fas fa-times"></i>
+          </div>
+        </div>)
+        })}
+       {toDos.map((obj)=>{
+        if(obj.status){
+          return( <h1>{obj.text}</h1>)
+        }
+       })}
+      </div>
     </div>
   );
 }
-
 export default App;
